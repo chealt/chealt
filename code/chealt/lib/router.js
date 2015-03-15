b9764f0fe6b38aa4f1425eac8a71756222f5e462
atikenny <atikenny@gmail.com>
2015-03-15 21:54:57 +0100

    not found template, workout item initial template, workout routers

--- code/chealt/lib/router.js
+++ code/chealt/lib/router.js
@@ -1,9 +1,17 @@
 Router.configure({
     layoutTemplate: 'layout',
     loadingTemplate: 'loading',
+    notFoundTemplate: 'notFound',
     waitOn: function () {
         return Meteor.subscribe('workouts');
     }
 });
 
 Router.route('/', { name: 'home' });
+Router.route('/workouts', { name: 'workoutsList' });
+Router.route('/workouts/:_id', {
+    name: 'workoutItem',
+    data: function () {
+        return Workouts.findOne(this.params._id);
+    }
+});
