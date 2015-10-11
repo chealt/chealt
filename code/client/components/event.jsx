1257ee47e278dd8a7c40334f6183377c0e57b944
atikenny <atikenny@gmail.com>
2015-10-11 16:58:47 +0200

    event card and format time component

--- code/client/components/event.jsx
+++ code/client/components/event.jsx
@@ -5,11 +5,25 @@ Event = React.createClass({
     render() {
         return (
             <div className="card">
-                <div className="name">{this.props.event.name}</div>
-                <div className="host">{this.props.event.host}</div>
-                <div className="time-container">
-                    <div className="start">{this.props.event.start.toTimeString()}</div>
-                    <div className="end">{this.props.event.end.toTimeString()}</div>
+                <h2 className="title separated">{this.props.event.name}</h2>
+                <div className="content-header">
+                    <div className="host">{this.props.event.host}</div>
+                    <div className="time-container">
+                        <div className="start">
+                            <FormatTime date={this.props.event.start} />
+                        </div>
+                        <div className="end">
+                            <FormatTime date={this.props.event.end} />
+                        </div>
+                    </div>
+                    <div className="location">{this.props.event.location}</div>
+                </div>
+                <div className="guests-container">
+                    <ul>
+                        {this.props.event.guests.map(function (guest) {
+                            return <li key={guest.name}>{guest.name}</li>;
+                        })}
+                    </ul>
                 </div>
             </div>
         );
---
+++ code/client/components/helpers/format-time.jsx
@@ -0,0 +1,11 @@
+FormatTime = React.createClass({
+    render() {
+        var paddedHours = '0' + this.props.date.getHours(),
+            paddedMinutes = '0' + this.props.date.getMinutes(),
+            formattedTime = paddedHours.substr(-2) + ':' + paddedMinutes.substr(-2);
+
+        return (
+            <span>{formattedTime}</span>
+        );
+    }
+});
