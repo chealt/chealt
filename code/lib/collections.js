5b8bcb8659c36eaabc70024beef671a8ec5f6c37
Attila Bartha <atikenny@gmail.com>
2015-10-20 09:18:05 +0200

    meteor update and events collection permissions

--- code/lib/collections.js
+++ code/lib/collections.js
@@ -1,1 +1,13 @@
 Events = new Mongo.Collection('events');
+
+Events.allow({
+    insert: function () {
+        return true;
+    },
+    update: function () {
+        return true;
+    },
+    remove: function () {
+        return true;
+    }
+});
