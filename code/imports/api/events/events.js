b1ee1fb7877256b1f119e16c7aa93c20f2f69458
atikenny <atikenny@gmail.com>
2016-10-31 20:03:54 +0100

    private, public and shared event publications

--- code/imports/api/events/events.js
+++ code/imports/api/events/events.js
@@ -2,26 +2,6 @@ import { Mongo } from 'meteor/mongo';
 
 export const Events = new Mongo.Collection('events');
 
-const isSharedWithUser = (event, userId) => {
-    return event.sharedWith && event.sharedWith.find((user) => {
-        return user._id === userId;
-    });
-};
-
-Events.allow({
-    insert: function (userId) {
-        return userId;
-    },
-    update: function (userId, document) {
-        const isHost = userId === document.host._id;
-
-        return document.isPublic || isHost || isSharedWithUser(document, userId);
-    },
-    remove: function (userId, document) {
-        return userId === document.host._id;
-    }
-});
-
 Events.publicFields = {
     id: 1,
     location: 1,
--- code/imports/api/events/server/publications.js
+++ code/imports/api/events/server/publications.js
@@ -4,9 +4,11 @@ import { Mongo } from 'meteor/mongo';
 import { Events } from '../events.js';
 
 Meteor.publish('events', function eventsPublication() {
-    /*if (!this.userId) {
-        return this.ready();
-    }*/
-
-    return Events.find();
+    return Events.find({
+        $or: [
+            { isPublic: true },
+            { "host._id": this.userId },
+            { sharedWith: { $elemMatch: { _id: this.userId } } }
+        ]
+    });
 });
