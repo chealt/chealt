0f3e0f8610aacae5dfffbd73ce54a7fe6c0eddc6
Attila Bartha <atikenny@gmail.com>
2018-09-15 14:31:03 +0100

    add watch script

--- packages/ui/config/webpack.config.js
+++ packages/ui/config/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
+const buildFolder = path.resolve(__dirname, 'dist');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: buildFolder
     }
 };
