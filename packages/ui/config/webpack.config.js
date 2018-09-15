377851aa43a351d8fd333dba1b6eab2c50f2bd1b
Attila Bartha <atikenny@gmail.com>
2018-09-15 21:36:46 +0100

    add mode to webpack config

--- packages/ui/config/webpack.config.js
+++ packages/ui/config/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 
-const buildFolder = path.resolve(__dirname, 'dist');
+const buildFolder = path.resolve(__dirname, '../dist');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     entry: './src/index.js',
+    devServer: {
+        contentBase: buildFolder,
+        compress: true,
+        port: 9000
+    },
     devtool: 'cheap-eval-source-map',
+    mode: isProduction ? 'production' : 'development',
     output: {
         filename: 'main.js',
         path: buildFolder
