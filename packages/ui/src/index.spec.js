b862db2d902b4fd189ecff031e70c6bd65ddf94f
Attila Bartha <atikenny@gmail.com>
2018-10-07 17:27:52 +0100

    create visit page method for tests

--- packages/ui/src/index.spec.js
+++ packages/ui/src/index.spec.js
@@ -3,11 +3,13 @@ import getConfig from './get-config';
 const { HOMEPAGE_URL } = getConfig();
 
 describe('Homepage', () => {
-    beforeAll(async () => {
+    const visitPage = async () => {
         await page.goto(`${HOMEPAGE_URL}`);
-    });
+    };
 
     it('has the proper title', async () => {
+        await visitPage();
+
         const title = await page.title();
 
         expect(title).toEqual('Chealt');
