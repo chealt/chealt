b350209e74226d94fc020fc1912cd1815e811a31
Attila Bartha <atikenny@gmail.com>
2018-10-13 20:24:13 +0100

    use require since apollo-server does not support node modules

--- packages/data-server/src/index.js
+++ packages/data-server/src/index.js
@@ -1,4 +1,4 @@
-import { ApolloServer, gql } from 'apollo-server';
+const { ApolloServer, gql } = require('apollo-server');
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
