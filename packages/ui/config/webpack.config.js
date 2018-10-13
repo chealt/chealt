8eb1ef8f001e978aca2c4e7833e9ca73d53d48ee
Attila Bartha <atikenny@gmail.com>
2018-10-13 20:24:25 +0100

    fix linting error

--- packages/ui/config/webpack.config.js
+++ packages/ui/config/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const buildFolder = path.resolve(__dirname, '../dist');
 
