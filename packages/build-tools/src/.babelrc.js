40e0eaa3567f81e653a876476be3b4c85c57ebdc
Attila Bartha <attila.bartha@babylonhealth.com>
2019-08-26 19:07:54 +0100

    add babel plugins

--- packages/build-tools/src/.babelrc.js
+++ packages/build-tools/src/.babelrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     "plugins": [
-        "@babel/plugin-proposal-object-rest-spread"
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-transform-async-to-generator",
+        "@babel/plugin-transform-runtime"
     ],
     "presets": [
         "@babel/preset-env",
