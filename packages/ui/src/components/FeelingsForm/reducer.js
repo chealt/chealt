dc5b88c8befed89400c742cfb982ac5e2d1350b1
Attila Bartha <attila.bartha@babylonhealth.com>
2019-09-08 19:27:55 +0100

    preped new feeling, sort feeling when loading items

--- packages/ui/src/components/FeelingsForm/reducer.js
+++ packages/ui/src/components/FeelingsForm/reducer.js
@@ -1,17 +1,19 @@
 const defaultState = [];
 
+const sortById = (a, b) => a.id - b.id;
+
 const feelings = (state = defaultState, action) => {
     switch (action.type) {
         case 'FEELINGS.ADD':
             return [
-                ...state,
                 {
                     id: Date.now(),
                     feeling: action.feeling
-                }
+                },
+                ...state
             ];
         case 'FEELINGS.LOAD':
-            return [...state, action.feelings];
+            return [...state, action.feelings.sort(sortById)];
         default:
             return state;
     }
