43c16ddd5f4b2a6ad497114ad654f08ce1bbbd4c
Attila Bartha <attila.bartha@babylonhealth.com>
2019-09-08 18:39:35 +0100

    apply margin to the Header

--- packages/ui/src/components/Header/index.jsx
+++ packages/ui/src/components/Header/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { node } from 'prop-types';
 
-const Header = ({ children }) => <header>{children}</header>;
+const Header = ({ children }) => (
+    <header className="margin--l">{children}</header>
+);
 
 Header.propTypes = {
     children: node.isRequired
