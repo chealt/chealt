5e76e5802782262b98e18a461f3face30808d178
Attila Bartha <attila.bartha@babylonhealth.com>
2019-09-14 21:20:47 +0100

    create url and request options utils

--- packages/ui/src/components/Google/GoogleFit/index.js
+++ packages/ui/src/components/Google/GoogleFit/index.js
@@ -1,15 +1,14 @@
 import { observeStore } from '../../redux-utils';
 import { googleAccessTokenSelector } from '../../Authentication/selectors';
-import { API_URL, DATA_SOURCES_PATH } from './constants';
-import { getHeaders } from './utils';
+import { getRequestOptions, getDataSourceUrl } from './utils';
 
 const fetchJSON = (...props) =>
     fetch(...props).then((response) => response.json());
 
 const loadData = async (accessToken) => {
     const data = await fetchJSON(
-        `${API_URL}/${DATA_SOURCES_PATH}?startTime=2019-09-13T00:00:00.000Z&endTime=2019-09-14T23:59:59.999Z`,
-        { headers: getHeaders(accessToken) }
+        getDataSourceUrl(),
+        getRequestOptions(accessToken)
     );
 
     console.log(data); // eslint-disable-line no-console
