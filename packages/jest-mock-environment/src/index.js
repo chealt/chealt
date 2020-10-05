3cb6eba6c8ab792661c7aa90fd176a9775c5cce3
Attila Bartha <Attila.Bartha@ibm.com>
2020-10-05 17:35:07 +0200

    specify main in package.json, create jest mock env

---
+++ packages/jest-mock-environment/src/index.js
@@ -0,0 +1,71 @@
+const PuppeteerEnvironment = require("jest-environment-puppeteer");
+const path = require("path");
+
+const factory = require("./factory");
+const { getLogger, logLevels } = require("./logger");
+const { addTestResponses, setResponsesPath } = require("./state");
+
+const { MOCK, DEBUG } = process.env;
+
+const isTestStartEvent = (event) => event.name === "test_fn_start";
+const isTestsEndEvent = (event) => event.name === "teardown";
+const getTestID = (event) => {
+    let id = event.name;
+
+    if (event.parent && event.parent.name !== "ROOT_DESCRIBE_BLOCK") {
+        const parentID = getTestID(event.parent); // eslint-disable-line no-unused-vars
+        id = `${parentID}/${id}`;
+    }
+
+    return id;
+};
+
+class MockEnvironment extends PuppeteerEnvironment {
+    constructor(config) {
+        super(config);
+
+        const responsesPath = path.join(
+            config.rootDir,
+            config.testEnvironmentOptions.mockResponsePath
+        );
+        setResponsesPath(responsesPath);
+        // eslint-disable-next-line import/no-dynamic-require, global-require
+        this.mocks = MOCK && require(responsesPath);
+    }
+
+    static shouldSaveResponses(responses) {
+        return !MOCK && Object.keys(responses).length;
+    }
+
+    async setup({
+        logger = getLogger(DEBUG ? logLevels.debug : logLevels.default)
+    } = {}) {
+        await super.setup();
+        // Your setup
+        this.logger = logger;
+        this.envInstance = await factory({
+            page: this.global.page,
+            mocks: this.mocks,
+            logger
+        });
+    }
+
+    async handleTestEvent(event) {
+        if (isTestStartEvent(event)) {
+            const testID = getTestID(event.test);
+            this.envInstance.setTestName(testID);
+        } else if (isTestsEndEvent(event)) {
+            this.addTestResponses();
+        }
+    }
+
+    addTestResponses() {
+        const responses = this.envInstance.getResponses();
+
+        if (MockEnvironment.shouldSaveResponses(responses)) {
+            addTestResponses(responses);
+        }
+    }
+}
+
+module.exports = MockEnvironment;
---
+++ packages/jest-mock-environment/src/logger.js
@@ -0,0 +1,41 @@
+const logLevels = {
+    debug: 1,
+    info: 2,
+    warning: 3,
+    error: 4,
+    //
+    default: 3
+};
+
+const getLogger = (level) => {
+    const noop = () => null;
+
+    return {
+        debug:
+            level <= logLevels.debug
+                ? (message) => {
+                      console.debug(message);
+                  }
+                : noop,
+        info:
+            level <= logLevels.info
+                ? (message) => {
+                      console.info(message);
+                  }
+                : noop,
+        warning:
+            level <= logLevels.warning
+                ? (message) => {
+                      console.warning(message);
+                  }
+                : noop,
+        error:
+            level <= logLevels.error
+                ? (message) => {
+                      console.error(message);
+                  }
+                : noop
+    };
+};
+
+module.exports = { getLogger, logLevels };
