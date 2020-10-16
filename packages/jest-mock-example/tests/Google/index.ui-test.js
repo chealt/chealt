8ab7f8db90cdafbf6c67ec9f8b1d3ac30ae82d11
Attila Bartha <Attila.Bartha@ibm.com>
2020-10-16 17:19:30 +0200

    add new spec to example

--- packages/jest-mock-example/tests/Google/index.ui-test.js
+++ packages/jest-mock-example/tests/Google/index.ui-test.js
@@ -11,4 +11,15 @@ describe('Google', () => {
     // THEN
     await expect(page).toMatchElement('img[alt=Google]');
   });
+
+  it('should have an input field for searching', async () => {
+    // GIVEN
+    await agreeToConsent();
+
+    // WHEN
+    await page.goto('https://google.com');
+
+    // THEN
+    await expect(page).toFill('input[name=q]', 'search term');
+  });
 });
