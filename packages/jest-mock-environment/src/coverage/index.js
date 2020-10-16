b0531e99f3c99c7990ba196c80d9eb47bf8cecd4
Attila Bartha <Attila.Bartha@ibm.com>
2020-10-16 15:51:11 +0200

    add coverage utilities

---
+++ packages/jest-mock-environment/src/coverage/index.js
@@ -0,0 +1,39 @@
+const startCollecting = (page) => Promise.all([
+  page.coverage.startJSCoverage(),
+  page.coverage.startCSSCoverage()
+]);
+
+const getCoverage = async (page) => {
+  const [jsCoverage, cssCoverage] = await Promise.all([
+    page.coverage.stopJSCoverage(),
+    page.coverage.stopCSSCoverage()
+  ]);
+  let totalBytes = 0;
+  let usedBytes = 0;
+  const coverage = [...jsCoverage, ...cssCoverage];
+
+  for (const entry of coverage) {
+    totalBytes += entry.text.length;
+
+    for (const range of entry.ranges) {
+      usedBytes += range.end - range.start - 1;
+    }
+  }
+
+  const percentage = usedBytes / totalBytes * 100;
+
+  return {
+    usedBytes,
+    totalBytes,
+    percentage,
+    coverageDetails: {
+      js: jsCoverage,
+      css: cssCoverage
+    }
+  };
+};
+
+export {
+  startCollecting,
+  getCoverage
+};
