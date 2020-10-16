7e65a314b4a540fc5f204f17e78ae21fea0d7aec
Attila Bartha <Attila.Bartha@ibm.com>
2020-10-16 11:15:04 +0200

    implement host agnostic mock matching

--- packages/jest-mock-environment/src/mockUtils.js
+++ packages/jest-mock-environment/src/mockUtils.js
@@ -1,8 +1,19 @@
 const removePort = (url) => url.replace(/:\d\d\d\d[\d]*/gu, '');
+const removeHost = (url) => url.replace(/https?:\/\/[^/]*/gu, '');
 
-const findMocksForUrl = ({ isPortAgnostic }) => (mocks, url) => {
+const findMocksForUrl = ({ isPortAgnostic, isHostAgnostic }) => (mocks, url) => {
   const mockKey = Object.keys(mocks).find(
-    (responseUrl) => (!isPortAgnostic ? responseUrl === url : removePort(responseUrl) === removePort(url))
+    (responseUrl) => {
+      if (isPortAgnostic) {
+        return removePort(responseUrl) === removePort(url);
+      }
+
+      if (isHostAgnostic) {
+        return removeHost(responseUrl) === removeHost(url);
+      }
+
+      return responseUrl === url;
+    }
   );
 
   if (mockKey) {
