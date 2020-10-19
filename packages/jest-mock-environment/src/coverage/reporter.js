627f9aeba4dd15f38ec188e863e360729915ea81
Attila Bartha <Attila.Bartha@ibm.com>
2020-10-19 14:47:44 +0200

    collect summary details for coverage

--- packages/jest-mock-environment/src/coverage/reporter.js
+++ packages/jest-mock-environment/src/coverage/reporter.js
@@ -1,10 +1,26 @@
-const getDetailsSummary = (details) => details;
+const formatSize = (size) => `${(size / 1024 / 1024).toFixed(2)} MB`;
+const formatRawSize = (size) => new Intl.NumberFormat().format(size);
+const formatPercentage = (percentage) => `${percentage.toFixed(2)}%`;
+const getDetailsSummary = (details) => {
+  const { totalBytes, usedBytes, percentage } = details;
+  const totalFormatted = formatSize(totalBytes);
+  const totalRawFormatted = formatRawSize(totalBytes);
+  const totalReport = `${totalFormatted} (${totalRawFormatted} B)`;
+  const usedFormatted = formatSize(usedBytes);
+  const usedRawFormatted = formatRawSize(usedBytes);
+  const usedReport = `${usedFormatted} (${usedRawFormatted} B)`;
+  const percentageFormatted = formatPercentage(percentage);
 
-const printReport = (details, logger) => {
-  logger.info(details);
+  return {
+    totalFormatted,
+    totalReport,
+    usedFormatted,
+    usedReport,
+    percentageFormatted,
+    ...details
+  };
 };
 
 module.exports = {
-  getDetailsSummary,
-  printReport
+  getDetailsSummary
 };
