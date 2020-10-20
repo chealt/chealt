bd4b965d1aa46966ff9905f347788de1fdb376db
Attila Bartha <Attila.Bartha@ibm.com>
2020-10-20 15:54:39 +0200

    pretty print coverage summary using chalk

--- packages/jest-mock-environment/src/coverage/utils.js
+++ packages/jest-mock-environment/src/coverage/utils.js
@@ -1,3 +1,5 @@
+const { bold, grey, inverse } = require('chalk');
+
 const filterByUrl = (collectCoverageFrom) => ({ url }) => {
   const shouldCollectCoverage = collectCoverageFrom.some((urlRegex) => new RegExp(urlRegex, 'u').test(url));
 
@@ -37,15 +39,16 @@ const calculateCoverageDetails = (jsCoverage, cssCoverage) => {
 };
 
 const printCoverages = ({ logger, coverages }) => {
+  const prefix = inverse.bold(' COVERAGE ');
   Object.keys(coverages).forEach((testID) => {
     const { report } = coverages[testID];
 
     logger.info(`
-${testID}:
-  Total: ${report.totalReport}
-  Unused: ${report.unusedReport}
-  Potential improvement: ${report.timeSavings.GGFormatted} (2G), ${report.timeSavings.GGGFormatted} (3G)
-    `);
+  ${grey(testID)}:
+    ${bold('Total:     ')} ${report.totalReport}
+    ${bold('Unused:    ')} ${report.unusedReport}
+    ${bold('Could save:')} ${report.timeSavings.GGGFormatted} (3G), ${report.timeSavings.GGFormatted} (2G)
+    `, prefix);
   });
 };
 
