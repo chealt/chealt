c8fa80afa3a9afc30d3f119f8273722fa7a2582b
Attila Bartha <Attila.Bartha@ibm.com>
2021-01-08 16:47:53 +0100

    add dark theme, cleanup brand colors

--- packages/fitter/src/components/header/index.js
+++ packages/fitter/src/components/header/index.js
@@ -4,9 +4,8 @@ import style from './style.css';
 
 const Header = () => (
   <header class={style.header}>
-    <h1>Fitter</h1>
     <nav>
-      <Link activeClassName={style.active} href="/">Home</Link>
+      <Link activeClassName={style.active} href="/">Login</Link>
     </nav>
   </header>
 );
--- packages/fitter/src/index.js
+++ packages/fitter/src/index.js
@@ -1,3 +1,5 @@
+import './style/themes/dark.css';
+import './style/index.css';
 import App from './components/app';
 
 export default App;
