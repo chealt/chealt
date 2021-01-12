ac8e61769dcdaf57a4a611217db9e3b75ed91bdc
Attila Bartha <Attila.Bartha@ibm.com>
2021-01-12 21:26:44 +0100

    add google sign-in with fitness activity scope

--- packages/fitter/src/components/header/index.js
+++ packages/fitter/src/components/header/index.js
@@ -1,13 +1,48 @@
 import { h } from 'preact';
-import { Link } from 'preact-router/match';
+import { useEffect, useState } from 'preact/hooks';
 import style from './style.css';
 
-const Header = () => (
-  <header class={style.header}>
-    <nav>
-      <Link activeClassName={style.active} href="/">Login</Link>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [isLoginOpen, setLoginOpen] = useState(false);
+
+  const renderGoogleSignIn = () => {
+    const onSuccess = (googleUser) => {
+      console.log(`Logged in as: ${googleUser.getBasicProfile().getName()}`);
+    };
+
+    const onFailure = (error) => {
+      console.log(error);
+    };
+
+    window.gapi.signin2.render('google-signin', {
+      scope: 'profile email https://www.googleapis.com/auth/fitness.activity.read',
+      longtitle: true,
+      theme: 'dark',
+      onsuccess: onSuccess,
+      onfailure: onFailure
+    });
+  };
+
+  useEffect(() => {
+    document.querySelector('#googleScript').addEventListener('load', () => {
+      renderGoogleSignIn();
+    });
+  });
+
+  return (
+    <header class={style.header}>
+      <nav>
+        <button onClick={() => {
+          setLoginOpen(!isLoginOpen);
+        }}>Login</button>
+      </nav>
+      <ul style={{display: isLoginOpen ? 'block' : 'none'}}>
+        <li>
+          <div id="google-signin" />
+        </li>
+      </ul>
+    </header>
+  );
+}
 
 export default Header;
