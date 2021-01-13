dad69caa20ba812eca9c72fb05a922d404421204
Attila Bartha <Attila.Bartha@ibm.com>
2021-01-13 09:57:09 +0100

    load fit sessions to local state

--- packages/fitter/src/components/header/index.js
+++ packages/fitter/src/components/header/index.js
@@ -2,7 +2,15 @@ import { h } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
 import style from './style.css';
 
+const loadSessions = (accessToken) => fetch("https://www.googleapis.com/fitness/v1/users/me/sessions", {
+  mode: 'cors',
+  headers: {
+    Authorization: `Bearer ${accessToken}`
+  }
+}).then((response) => response.json());
+
 const Header = () => {
+  const [sessions, setSessions] = useState();
   const [isLoadingAuth, setLoadingAuth] = useState(true);
   const [user, setUser] = useState();
   const [isLoginOpen, setLoginOpen] = useState(false);
@@ -11,13 +19,14 @@ const Header = () => {
     throw new Error(error);
   };
   const userName = user && user.getBasicProfile().getName();
+  const accessToken = user && user.getAuthResponse(true).access_token;
 
   const renderGoogleSignIn = () => {
     window.gapi.signin2.render('google-signin', {
       scope: 'profile email https://www.googleapis.com/auth/fitness.activity.read',
       longtitle: true,
       theme: 'dark',
-      onsuccess: (user) => {
+      onsuccess: async (user) => {
         setLoadingAuth(false);
         setUser(user);
         setLoginOpen(false);
@@ -31,6 +40,23 @@ const Header = () => {
 
     googlePlatformScript.addEventListener('load', renderGoogleSignIn);
   });
+
+  useEffect(() => {
+    if (accessToken) {
+      (async () => {
+        const sessions = await loadSessions(accessToken);
+
+        setSessions(sessions.session.map(((session) => ({
+          ...session,
+          startTime: new Date(parseInt(session.startTimeMillis, 10))
+        }))))
+      })()
+    }
+  }, [accessToken]);
+
+  if (sessions) {
+    console.log(sessions);
+  }
 
   return (
     <header class={style.header}>
