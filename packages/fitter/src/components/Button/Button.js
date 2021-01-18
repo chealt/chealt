fcb088c90797583bcef290954c7c4191894c8b6f
Attila Bartha <Attila.Bartha@ibm.com>
2021-01-18 16:24:48 +0100

    add ghost Button type

--- packages/fitter/src/components/Button/Button.js
+++ packages/fitter/src/components/Button/Button.js
@@ -2,8 +2,8 @@ import { h } from 'preact';
 
 import style from './style.css';
 
-const Button = ({ size, children, ...rest }) => (
-  <button class={size ? style[size] : undefined} {...rest}>{children}</button>
+const Button = ({ size, type, children, ...rest }) => (
+  <button class={`${size ? style[size] : ''} ${type ? style[type] : ''}`} {...rest}>{children}</button>
 );
 
 export default Button;
