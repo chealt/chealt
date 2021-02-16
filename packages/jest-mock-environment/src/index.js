51d4f4afd2aba99914e2cae2c0d550424af7cc99
Attila Bartha <Attila.Bartha@ibm.com>
2021-02-16 10:49:12 +0100

    fix collecting perf metrics when config is off

--- packages/jest-mock-environment/src/index.js
+++ packages/jest-mock-environment/src/index.js
@@ -109,42 +109,64 @@ class MockEnvironment extends PuppeteerEnvironment {
   }
 
   async handleTestEvent(event) {
+    if (isTestStartEvent(event)) {
+      await this.handleStartEvent(event);
+    } else if (isTestsEndEvent(event)) {
+      await this.handleTestsEndEvent();
+    } else if (isTestEndEvent(event)) {
+      await this.handleTestEndEvent();
+    } else if (isTestFailureEvent(event)) {
+      await this.handleTestFailEvent();
+    }
+  }
+
+  async handleStartEvent(event) {
     const { collectCoverage, recordScreenshots } = this.config;
 
-    if (isTestStartEvent(event)) {
-      const testID = getTestID(event.test);
-      this.envInstance.setTestName(testID);
+    const testID = getTestID(event.test);
+    this.envInstance.setTestName(testID);
 
-      if (collectCoverage) {
-        this.envInstance.startCollectingCoverage();
-      }
-
-      await this.envInstance.startInterception();
-
-      if (recordScreenshots) {
-        await this.envInstance.startRecording();
-      }
-    } else if (isTestsEndEvent(event)) {
-      this.addTestResponses();
-
-      if (collectCoverage) {
-        await this.addCodeCoverages();
-      }
-    } else if (isTestEndEvent(event)) {
-      await this.envInstance.stopInterception();
-
-      if (collectCoverage) {
-        await this.envInstance.stopCollectingCoverage();
-      }
-
-      if (recordScreenshots) {
-        await this.envInstance.stopRecording(this.screenshotFullPath);
-      }
-
-      await savePerformanceMetrics(await this.envInstance.getMetrics());
-    } else if (isTestFailureEvent(event)) {
-      await this.envInstance.takeScreenshot(this.screenshotFullPath);
+    if (collectCoverage) {
+      this.envInstance.startCollectingCoverage();
     }
+
+    await this.envInstance.startInterception();
+
+    if (recordScreenshots) {
+      await this.envInstance.startRecording();
+    }
+  }
+
+  async handleTestsEndEvent() {
+    const { collectCoverage } = this.config;
+
+    this.addTestResponses();
+
+    if (collectCoverage) {
+      await this.addCodeCoverages();
+    }
+  }
+
+  async handleTestEndEvent() {
+    const { collectCoverage, recordScreenshots, collectPerfMetrics } = this.config;
+
+    await this.envInstance.stopInterception();
+
+    if (collectCoverage) {
+      await this.envInstance.stopCollectingCoverage();
+    }
+
+    if (recordScreenshots) {
+      await this.envInstance.stopRecording(this.screenshotFullPath);
+    }
+
+    if (collectPerfMetrics) {
+      await savePerformanceMetrics(await this.envInstance.getMetrics());
+    }
+  }
+
+  handleTestFailEvent() {
+    return this.envInstance.takeScreenshot(this.screenshotFullPath);
   }
 
   addTestResponses() {
