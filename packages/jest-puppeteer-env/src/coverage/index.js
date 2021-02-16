5499e26052b40256dc587e68190fc32ae41bdc33
Attila Bartha <Attila.Bartha@ibm.com>
2021-02-16 20:44:15 +0100

    chore: move teardown and state

---
+++ packages/jest-puppeteer-env/src/coverage/index.js
@@ -0,0 +1,49 @@
+const { filterByUrl, removeCoverageText, calculateCoverageDetails } = require('./utils');
+const { getDetailsReport } = require('./reporter');
+
+const startCollecting = (page) => Promise.all([page.coverage.startJSCoverage(), page.coverage.startCSSCoverage()]);
+
+const getCoverage = async ({ page, collectCoverageFrom, recordCoverageText }) => {
+  const [jsCoverage, cssCoverage] = await Promise.all([
+    page.coverage.stopJSCoverage(),
+    page.coverage.stopCSSCoverage()
+  ]);
+  let finalJSCoverage = jsCoverage;
+  let finalCSSCoverage = cssCoverage;
+
+  if (collectCoverageFrom) {
+    const byUrl = filterByUrl(collectCoverageFrom);
+    finalJSCoverage = finalJSCoverage.filter(byUrl);
+    finalCSSCoverage = finalCSSCoverage.filter(byUrl);
+  }
+
+  const { usedBytes, totalBytes, percentage } = calculateCoverageDetails(finalJSCoverage, finalCSSCoverage);
+
+  // This must be done after the calculate method as that depends on the text length.
+  if (!recordCoverageText) {
+    finalJSCoverage = finalJSCoverage.map(removeCoverageText);
+    finalCSSCoverage = finalCSSCoverage.map(removeCoverageText);
+  }
+
+  const details = {
+    usedBytes,
+    totalBytes,
+    percentage,
+    coverageDetails: {
+      js: finalJSCoverage,
+      css: finalCSSCoverage
+    }
+  };
+
+  const report = getDetailsReport(details);
+
+  return {
+    ...details,
+    ...report
+  };
+};
+
+module.exports = {
+  startCollecting,
+  getCoverage
+};
---
+++ packages/jest-puppeteer-env/src/coverage/utils.js
@@ -0,0 +1,63 @@
+const { bold, grey, inverse } = require('chalk');
+
+const filterByUrl = (collectCoverageFrom) => ({ url }) => {
+  const shouldCollectCoverage = collectCoverageFrom.some((urlRegex) => new RegExp(urlRegex, 'u').test(url));
+
+  return shouldCollectCoverage;
+};
+
+const removeCoverageText = (coverage) => {
+  const mappedCoverage = {
+    ...coverage
+  };
+
+  delete mappedCoverage.text;
+
+  return mappedCoverage;
+};
+
+const calculateCoverageDetails = (jsCoverage, cssCoverage) => {
+  let totalBytes = 0;
+  let usedBytes = 0;
+  const coverage = [...jsCoverage, ...cssCoverage];
+
+  for (const entry of coverage) {
+    totalBytes += entry.text.length;
+
+    for (const range of entry.ranges) {
+      usedBytes += range.end - range.start - 1;
+    }
+  }
+
+  const percentage = (usedBytes / totalBytes) * 100;
+
+  return {
+    usedBytes,
+    totalBytes,
+    percentage
+  };
+};
+
+const printCoverages = ({ logger, coverages }) => {
+  const prefix = inverse.bold(' COVERAGE ');
+  Object.keys(coverages).forEach((testID) => {
+    const { report } = coverages[testID];
+
+    logger.info(
+      `
+  ${grey(testID)}:
+    ${bold('Total:     ')} ${report.totalReport}
+    ${bold('Unused:    ')} ${report.unusedReport}
+    ${bold('Could save:')} ${report.timeSavings.GGGFormatted} (3G), ${report.timeSavings.GGFormatted} (2G)
+    `,
+      prefix
+    );
+  });
+};
+
+module.exports = {
+  calculateCoverageDetails,
+  filterByUrl,
+  printCoverages,
+  removeCoverageText
+};
