966a4aa2271820359fa075d36c6e133396262603
Attila Bartha <Attila.Bartha@ibm.com>
2021-02-19 08:48:25 +0100

    remove jest-mock-environment package

--- packages/jest-mock-environment/src/setup.js
+++
@@ -1,8 +0,0 @@
-const { setup: setupPuppeteer } = require('@chealt/jest-puppeteer-env');
-
-const globalSetup = async (globalConfig) => {
-  await setupPuppeteer(globalConfig);
-  // Your global setup
-};
-
-module.exports = globalSetup;
