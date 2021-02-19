0f3881c4130e5eea11d66422a7e5d46a7c53a63d
Attila Bartha <Attila.Bartha@ibm.com>
2021-02-19 09:45:49 +0100

    add missing createDir file util

--- packages/jest-puppeteer-env/src/fileUtils.js
+++ packages/jest-puppeteer-env/src/fileUtils.js
@@ -5,7 +5,14 @@ const { promisify } = require('util');
 const writeFile = promisify(fs.writeFile);
 const mkdir = promisify(fs.mkdir);
 
+const createDir = (filepath) => {
+  const dirname = path.dirname(filepath);
+
+  return mkdir(dirname, { recursive: true });
+};
+
 const writeFileSafe = async (filepath, content) => {
+  await createDir(filepath);
   const dirname = path.dirname(filepath);
   await mkdir(dirname, { recursive: true });
 
@@ -13,5 +20,6 @@ const writeFileSafe = async (filepath, content) => {
 };
 
 module.exports = {
+  createDir,
   writeFileSafe
 };
