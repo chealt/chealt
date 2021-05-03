bde6ff67b9c45bd543cc2f3ef9d5bb5f892e42a6
Attila Bartha <atikenny@gmail.com>
2021-05-03 18:51:34 +0200

    add missing return value

--- packages/mocker/src/utils.js
+++ packages/mocker/src/utils.js
@@ -86,6 +86,8 @@ const findMocksForUrl = ({ isPortAgnostic, isHostAgnostic }) => ({ mocks, url, h
       }
     }
   }
+
+  return undefined;
 };
 
 export { getFolderAbsPath, getMocks, findMocksForUrl };
