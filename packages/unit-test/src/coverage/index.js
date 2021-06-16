3f4e9d7e6fca3e7e90cccf9154b78a1fc9bc0fe7
Attila Bartha <atikenny@gmail.com>
2021-06-16 08:22:48 +0200

    test cloud provider config

--- packages/unit-test/src/coverage/index.js
+++ packages/unit-test/src/coverage/index.js
@@ -1,9 +1,24 @@
-const uploadCoverage = ({
-  cloudProvider,
-  APIKey,
-  bucketName,
-  coverageFolder,
-  git: { domain, org, repo, branch, hash }
-}) => true;
+const validateIBMConfig = (config) => {};
+
+const validateCloudProviderConfig = (config) => {
+  if (!config) {
+    throw new Error('Please provide a cloud provider config.');
+  }
+
+  switch (config.provider) {
+    case 'IBM':
+      return validateIBMConfig(config);
+    default:
+      throw new Error(`Unknown cloud provider: "${config.provider}"`);
+  }
+};
+
+const uploadCoverage = (
+  { cloudProviderConfig, APIKey, bucketName, coverageFolder, git: { domain, org, repo, branch, hash } } = { git: {} }
+) => {
+  validateCloudProviderConfig(cloudProviderConfig);
+
+  return true;
+};
 
 export { uploadCoverage };
--- packages/unit-test/src/coverage/index.spec.js
+++ packages/unit-test/src/coverage/index.spec.js
@@ -3,9 +3,17 @@ import { uploadCoverage } from '.';
 describe('coverage module', () => {
   describe('uploadCoverage()', () => {
     it('uploads coverage folder to IBM Cloud', () => {
-      const result = uploadCoverage({ git: {} });
+      const result = uploadCoverage({ cloudProviderConfig: { provider: 'IBM' }, git: {} });
 
       expect(result).toBeTruthy();
+    });
+
+    it('should throw when cloud provider is unknown', () => {
+      expect(() => uploadCoverage({ cloudProviderConfig: {}, git: {} })).toThrow();
+    });
+
+    it('should throw when cloud provider config is not provided', () => {
+      expect(() => uploadCoverage({ git: {} })).toThrow();
     });
   });
 });
