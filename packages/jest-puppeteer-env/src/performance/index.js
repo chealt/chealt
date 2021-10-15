50d8294ddd49e9ee8a2ddc3d551f3dedfb9ce1a0
Attila Bartha <atikenny@gmail.com>
2021-10-15 14:33:36 +0200

    calculate performance summary

--- packages/jest-puppeteer-env/src/performance/index.js
+++ packages/jest-puppeteer-env/src/performance/index.js
@@ -1,4 +1,5 @@
 const { checkBundleSize: checkBundleSizeFactory } = require('./bundleSize');
+const { getMetricsSummary } = require('./utils');
 
 const performance = async ({ page, client, config }) => {
   const enabling = client.send('Performance.enable');
@@ -8,11 +9,16 @@ const performance = async ({ page, client, config }) => {
       page.evaluate(() => JSON.stringify(window.performance.getEntries())),
       enabling.then(() => client.send('Performance.getMetrics')).then(({ metrics }) => metrics),
       page.metrics()
-    ]).then(([entries, metrics, pageMetrics]) => ({
-      entries: JSON.parse(entries),
-      metrics,
-      pageMetrics
-    }));
+    ]).then(([rawEntries, metrics, pageMetrics]) => {
+      const entries = JSON.parse(rawEntries);
+
+      return {
+        entries,
+        metrics,
+        pageMetrics,
+        summary: getMetricsSummary({ entries })
+      };
+    });
 
   const checkBundleSize = config.performance?.bundleSizes
     ? checkBundleSizeFactory({ bundleSizes: config.performance.bundleSizes })
---
+++ packages/jest-puppeteer-env/src/performance/utils.js
@@ -0,0 +1,25 @@
+const getMetricsSummary = (metrics) => {
+  const summary = {};
+
+  if (metrics.entries) {
+    const firstPaint = metrics.entries.find((entry) => entry.name === 'first-paint');
+    const firstContentfulPaint = metrics.entries.find((entry) => entry.name === 'first-contentful-paint');
+
+    summary.firstPaint = firstPaint.startTime;
+    summary.firstContentfulPaint = firstContentfulPaint.startTime;
+
+    const navigationMetrics = metrics.entries.find((entry) => entry.entryType === 'navigation');
+
+    if (navigationMetrics) {
+      summary.domCompleted = navigationMetrics.duration;
+      summary.DNSLookup = navigationMetrics.domainLookupEnd - navigationMetrics.domainLookupStart;
+      summary.connectionTime = navigationMetrics.connectEnd - navigationMetrics.connectStart;
+      summary.responseTime = navigationMetrics.requestStart - navigationMetrics.responseEnd;
+      summary.domInteractive = navigationMetrics.domInteractive - navigationMetrics.responseEnd;
+    }
+  }
+
+  return summary;
+};
+
+module.exports = { getMetricsSummary };
