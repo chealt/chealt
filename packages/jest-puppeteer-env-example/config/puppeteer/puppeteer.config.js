665d90b72cb5bd24899d0c8607e96d928c52d5d2
Attila Bartha <atikenny@gmail.com>
2021-11-26 05:17:50 +0100

    add language arguments to example

--- packages/jest-puppeteer-env-example/config/puppeteer/puppeteer.config.js
+++ packages/jest-puppeteer-env-example/config/puppeteer/puppeteer.config.js
@@ -4,7 +4,8 @@ module.exports = {
   launch: {
     devtools: Boolean(DEBUG),
     headless: Boolean(CI),
-    slowMo: CI ? 0 : 10
+    slowMo: CI ? 0 : 10,
+    args: ['--lang=en-US']
   },
   browserContext: 'incognito'
 };
