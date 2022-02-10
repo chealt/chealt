e24e3557723be3ca9edd03d201a1a4a20afffe0d
Attila Bartha <atikenny@gmail.com>
2022-02-10 08:29:03 +0100

    initial puppeteer runner script

---
+++ packages/taas/src/AWS/Lambda/puppeteerRunner.js
@@ -0,0 +1,13 @@
+const handler = async (event, context) => {
+  console.log('in the handler');
+  console.log({ event });
+  console.log({ context });
+
+  return {
+    statusCode: 200
+  };
+};
+
+module.exports = {
+  handler
+};
