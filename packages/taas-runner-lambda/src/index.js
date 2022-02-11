cf4a189daac61939fd1440b24c56437de0462326
Attila Bartha <atikenny@gmail.com>
2022-02-11 19:04:47 +0100

    read file from S3

--- packages/taas-runner-lambda/src/index.js
+++ packages/taas-runner-lambda/src/index.js
@@ -1,7 +1,17 @@
-const handler = async (event, context) => {
-  console.log('in the handler');
-  console.log({ event });
-  console.log({ context });
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+
+const handler = async (event) => {
+  const { region, Bucket, Key } = event;
+  const client = new S3Client({
+    region
+  });
+  const command = new GetObjectCommand({
+    Bucket,
+    Key
+  });
+  const response = await client.send(command);
+
+  console.log(response.Body.read());
 
   return {
     statusCode: 200
