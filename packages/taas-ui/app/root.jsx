c67b4049ad482b2a1887a323bd3fd4d45d313e87
Attila Bartha <atikenny@gmail.com>
2022-02-20 06:42:24 +0100

    add taas-ui package

---
+++ packages/taas-ui/app/root.jsx
@@ -0,0 +1,22 @@
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix';
+
+export const meta = () => ({ title: 'New Remix App' });
+
+export default function App() {
+  return (
+    <html lang="en" className="spectrum spectrum--medium spectrum--light">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
