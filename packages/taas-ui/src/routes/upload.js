4f4f8ecbf00d6b9345a62278fb55f8e4bce1c20b
Attila Bartha <atikenny@gmail.com>
2022-03-01 08:09:30 +0100

    upload Puppeteer script to S3

--- packages/taas-ui/src/routes/upload.js
+++ packages/taas-ui/src/routes/upload.js
@@ -1,8 +1,32 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+const client = new S3Client({
+  region: 'eu-central-1'
+});
+
+const bucket = 'puppeteer-scripts';
+
 export const post = async ({ request }) => {
   const data = await request.formData();
+  // File object: https://developer.mozilla.org/en-US/docs/Web/API/File
+  const file = data.get('testScript');
+  const fileBuffer = await file.arrayBuffer();
+  const fileName = file.name;
 
-  console.log(data.get('accessToken'));
-  console.log(data.get('testScript'));
+  const putCommand = new PutObjectCommand({
+    Bucket: bucket,
+    Key: fileName,
+    Body: fileBuffer,
+    ContentType: 'text/javascript'
+  });
+
+  const filePath = `${bucket}/${fileName}`;
+
+  console.log(`Saving Puppeteer script to ${filePath}`);
+
+  await client.send(putCommand);
+
+  console.log(`File saved to ${filePath}`);
 
   return {
     status: 200
