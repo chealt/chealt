78b5fcb6afc427cc87d06ca4b108fdf03d923c76
Attila Bartha <atikenny@gmail.com>
2022-04-29 20:31:08 +0200

    add service workers

--- packages/chealt.com/public/index.js
+++ packages/chealt.com/public/index.js
@@ -3,6 +3,9 @@ import Home from './pages/home/index.js';
 import Documents from './pages/documents/index.js';
 import NotFound from './pages/_404.js';
 import Header from './Header/Header.js';
+import swURL from 'sw:./sw.js'; // eslint-disable-line import/no-unresolved
+
+navigator.serviceWorker.register(swURL);
 
 const App = () => (
   <LocationProvider>
