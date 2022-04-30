56465c04665e73c636cee7787ce38d590102249a
Attila Bartha <atikenny@gmail.com>
2022-04-30 19:34:58 +0200

    add Select and Option comp, add Sex input

--- packages/chealt.com/public/PersonalDetails/index.js
+++ packages/chealt.com/public/PersonalDetails/index.js
@@ -1,17 +1,30 @@
 import Form from '../Form/Form';
 import Input from '../Form/Input';
+import Option from '../Form/Option';
+import Select from '../Form/Select';
 import PageTitle from '../PageTitle';
 
 const PersonalDetails = () => (
   <>
     <PageTitle>Personal Details</PageTitle>
-    <Form>
+    <Form name="personalDetails">
+      <Input type="text" name="firstName">
+        First Name
+      </Input>
+      <Input type="text" name="lastName">
+        Last Name
+      </Input>
       <Input type="date" name="dateOfBirth">
         Date of Birth
       </Input>
       <Input type="email" name="email">
         Email
       </Input>
+      <Select name="sex" label="Sex">
+        <Option value="male">Male</Option>
+        <Option value="female">Female</Option>
+        <Option value="other">Other</Option>
+      </Select>
     </Form>
   </>
 );
