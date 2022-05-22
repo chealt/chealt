41c97649bf203581dfc6ffd4a51f5ce52037f60c
Attila Bartha <atikenny@gmail.com>
2022-05-22 14:26:14 +0200

    add project description

--- packages/chealt.com/public/Home/index.js
+++ packages/chealt.com/public/Home/index.js
@@ -1,5 +1,37 @@
 import PageTitle from '../PageTitle';
 
-const Home = () => <PageTitle>Chealt.com</PageTitle>;
+const Home = () => (
+  <>
+    <PageTitle>Chealt.com</PageTitle>
+    <h2>What</h2>
+    <p>This application helps you stay healthy and happy. Look around, you might see something useful.</p>
+    <h2>Privacy</h2>
+    <p>
+      All the data you enter into the application is stored on your device. There are no cookies, third-party
+      integrations, or anything like that. If there is a feature that requires you to send some data somewhere, we will
+      always ask for your permission and let you know about the consequences.
+    </p>
+    <h2>Improvements</h2>
+    <p>
+      If you have an idea how to make this app even more useful, let us know{' '}
+      <a
+        target="_blank"
+        href="https://github.com/chealt/chealt/issues/new?assignees=atikenny&labels=component:chealt.com&template=feature_request.md&title="
+        rel="noreferrer"
+      >
+        here
+      </a>{' '}
+      by creating a{' '}
+      <a
+        target="_blank"
+        href="https://docs.github.com/en/issues/tracking-your-work-with-issues/about-issues"
+        rel="noreferrer"
+      >
+        GitHub issue
+      </a>
+      .
+    </p>
+  </>
+);
 
 export default Home;
