44b78eeaf3270a275b3b00ad3b0291953b13c7ca
Attila Bartha <atikenny@gmail.com>
2022-05-29 06:07:54 +0200

    align open button

--- packages/chealt.com/public/Documents/Item.js
+++ packages/chealt.com/public/Documents/Item.js
@@ -1,9 +1,20 @@
+import Button from '../Form/Button';
 import Input from '../Form/Input';
+import Launch from '../Icons/Launch';
+
+import styles from './Item.module.css';
 
 const Item = ({ key, children, ...inputProps }) => (
-  <Input type="checkbox" value={key} {...inputProps}>
-    {children}
-  </Input>
+  <div class={styles.item}>
+    <div class={styles.content}>
+      <Input type="checkbox" value={key} {...inputProps}>
+        {children}
+      </Input>
+    </div>
+    <Button ghost>
+      <Launch />
+    </Button>
+  </div>
 );
 
 export default Item;
