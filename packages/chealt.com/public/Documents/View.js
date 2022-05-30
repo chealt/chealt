8d5f54f26562dec0ffe87fb7f301ad5172152540
Attila Bartha <atikenny@gmail.com>
2022-05-30 05:46:21 +0200

    view image document

--- packages/chealt.com/public/Documents/View.js
+++ packages/chealt.com/public/Documents/View.js
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'preact/hooks';
 import { useRoute } from 'preact-iso';
+
+import database from '../IndexedDB';
+import { getDocument } from './utils';
+
+import styles from './View.module.css';
 
 const View = () => {
   const {
     params: { documentKey }
   } = useRoute();
+  const [instance, setInstance] = useState();
+  const [imageSource, setImageSource] = useState();
 
-  console.log(documentKey);
+  useEffect(() => {
+    (async () => {
+      if (!instance) {
+        setInstance(await database({ database: 'chealt' }));
+      } else {
+        const { blob } = await getDocument(instance)({ documentKey });
 
-  return <div />;
+        const source = URL.createObjectURL(new Blob([blob]));
+
+        setImageSource(source);
+      }
+    })();
+  }, [instance, documentKey]);
+
+  return imageSource && <img src={imageSource} class={styles.image} />;
 };
 
 export default View;
