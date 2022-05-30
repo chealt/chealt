1bd9c9970f5b2cf40f4f66cb6532e4d7fdaa8480
Attila Bartha <atikenny@gmail.com>
2022-05-30 08:23:03 +0200

    improve Select and Button styles

--- packages/chealt.com/public/Form/Select.js
+++ packages/chealt.com/public/Form/Select.js
@@ -1,11 +1,15 @@
+import ArrowDown from '../Icons/ArrowDown';
 import styles from './Input.module.css';
 
 const Select = ({ children, label, ...selectProps }) => (
   <label class={styles.label}>
     <div class={styles.text}>{label}</div>
-    <select class={styles.select} {...selectProps}>
-      {children}
-    </select>
+    <div class={styles.selectWrapper}>
+      <select class={styles.select} {...selectProps}>
+        {children}
+      </select>
+      <ArrowDown class={styles.icon} />
+    </div>
   </label>
 );
 
---
+++ packages/chealt.com/public/Icons/ArrowDown.js
@@ -0,0 +1,8 @@
+const ArrowDown = ({ ...svgProps }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32" {...svgProps}>
+    <polygon points="16,22 6,12 7.4,10.6 16,19.2 24.6,10.6 26,12 " />
+    <rect id="_x3C_Transparent_Rectangle_x3E_" style="fill:none;" width="16" height="16" />
+  </svg>
+);
+
+export default ArrowDown;
