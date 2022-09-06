4785a71d8e23f5789aac0776b0256d6f48703674
Attila Bartha <atikenny@gmail.com>
2022-09-06 10:45:47 +0200

    get signed upload url

--- packages/upload-worker/src/index.js
+++ packages/upload-worker/src/index.js
@@ -1,5 +1,27 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+const uploadBucket = 'chealt-upload';
+
+const getJSONResponse = (response) =>
+  new Response(JSON.stringify(response), { headers: { 'content-type': 'application/json;charset=UTF-8' } });
+
 export default {
-  async fetch() {
-    return new Response('Hello World!');
+  async fetch(request, env) {
+    const { ACCOUNT_ID, ACCESS_KEY_ID, ACCESS_KEY_SECRET } = env;
+
+    const client = new S3Client({
+      region: 'us-east-1',
+      endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
+      credentials: {
+        accessKeyId: `${ACCESS_KEY_ID}`,
+        secretAccessKey: `${ACCESS_KEY_SECRET}`
+      },
+      signatureVersion: 'v4'
+    });
+    const command = new PutObjectCommand({ Bucket: uploadBucket, Key: 'test' });
+    const url = await getSignedUrl(client, command, { expiresIn: 3600 });
+
+    return getJSONResponse({ url });
   }
 };
