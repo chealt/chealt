55b9e74bbf99c5b3243fca472c7a3f6e96b5c7ad
Attila Bartha <atikenny@gmail.com>
2022-09-11 19:16:07 +0200

    add share steps and upload url request

--- packages/chealt.com/public/Share/index.js
+++ packages/chealt.com/public/Share/index.js
@@ -5,26 +5,39 @@ import ListItem from '../List/ListItem';
 import Controls from '../Form/Controls';
 import Link from '../Link';
 
-const Share = () => (
-  <>
-    <PageTitle>Share</PageTitle>
-    <p>
-      To Share your data with another device, follow these steps
-      <List>
-        <ListItem>Click on the Share button</ListItem>
-        <ListItem>Wait for the QR code to appear</ListItem>
-        <ListItem>
-          Open the <Link href="https://chealt.com/share">Chealt Share page</Link> on the device you want to share your
-          data with
-        </ListItem>
-        <ListItem>Click the Scan QR button</ListItem>
-      </List>
-    </p>
-    <Controls>
-      <Button emphasized>Share</Button>
-      <Button>Scan QR Code</Button>
-    </Controls>
-  </>
-);
+const uploadUrlHost = import.meta.env.UPLOAD_URL_HOST;
+
+const Share = () => {
+  const uploadContent = async () => {
+    const uploadUrlResponse = await fetch(uploadUrlHost);
+    const { url } = await uploadUrlResponse.json();
+
+    console.log(url);
+  };
+
+  return (
+    <>
+      <PageTitle>Share</PageTitle>
+      <p>
+        To Share your data with another device, follow these steps
+        <List>
+          <ListItem>Click on the Share button</ListItem>
+          <ListItem>Wait for the QR code to appear</ListItem>
+          <ListItem>
+            Open the <Link href="https://chealt.com/share">Chealt Share page</Link> on the device you want to share your
+            data with
+          </ListItem>
+          <ListItem>Click the Scan QR button</ListItem>
+        </List>
+      </p>
+      <Controls>
+        <Button emphasized onClick={uploadContent}>
+          Share
+        </Button>
+        <Button>Scan QR Code</Button>
+      </Controls>
+    </>
+  );
+};
 
 export default Share;
