5a31925152a664edfa950013290e68faa86510a7
Attila Bartha <atikenny@gmail.com>
2022-09-26 19:18:26 +0200

    focus on the first input when the modal is opened

--- packages/chealt.com/public/Modal/index.js
+++ packages/chealt.com/public/Modal/index.js
@@ -1,24 +1,34 @@
 import classnames from 'classnames';
+import { useEffect } from 'preact/hooks';
 
 import styles from './index.module.css';
 
-const Modal = ({ isOpen, close, children }) => (
-  <div
-    class={classnames({
-      [styles.modal]: true,
-      [styles.open]: Boolean(isOpen)
-    })}
-    onKeyUp={(event) => {
-      if (event.key === 'Escape') {
-        close();
-      }
-    }}
-  >
-    <span class={styles.close} onClick={close}>
-      X
-    </span>
-    {children}
-  </div>
-);
+const Modal = ({ isOpen, close, children }) => {
+  useEffect(() => {
+    if (isOpen) {
+      // focus the first input
+      document.forms[0][0].focus();
+    }
+  }, [isOpen]);
+
+  return (
+    <div
+      class={classnames({
+        [styles.modal]: true,
+        [styles.open]: Boolean(isOpen)
+      })}
+      onKeyUp={(event) => {
+        if (event.key === 'Escape') {
+          close();
+        }
+      }}
+    >
+      <span class={styles.close} onClick={close}>
+        X
+      </span>
+      {children}
+    </div>
+  );
+};
 
 export default Modal;
