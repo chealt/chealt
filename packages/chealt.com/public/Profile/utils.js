86e313303d2f65a1943c66d0c9aa051797177b8f
Attila Bartha <atikenny@gmail.com>
2022-10-16 19:49:36 +0200

    set default profile as selected

--- packages/chealt.com/public/Profile/utils.js
+++ packages/chealt.com/public/Profile/utils.js
@@ -1,5 +1,9 @@
 const createDefaultProfile = ({ instance }) =>
-  instance.save({ type: 'profiles', key: crypto.randomUUID(), value: { name: 'default' } });
+  instance.save({
+    type: 'profiles',
+    key: crypto.randomUUID(),
+    value: { name: 'default', isSelected: true }
+  });
 
 const initProfiles = async ({ instance }) => {
   const profiles = await instance.list({ type: 'profiles' });
