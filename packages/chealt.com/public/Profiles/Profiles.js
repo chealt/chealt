7762b4e34bcf04a6195352dccd38d52dd382d53c
Attila Bartha <atikenny@gmail.com>
2022-10-22 05:55:22 +0200

    remove the ability to rename a profile

--- packages/chealt.com/public/Profiles/Profiles.js
+++ packages/chealt.com/public/Profiles/Profiles.js
@@ -1,9 +1,7 @@
-import { useCallback, useRef, useState } from 'preact/hooks';
+import { useState } from 'preact/hooks';
 
 import Modal from '../Modal';
 import Button from '../Form/Button';
-import Form from '../Form/Form';
-import Input from '../Form/Input';
 import { useObjectStore } from '../IndexedDB/hooks';
 import Container from '../Layout/Container';
 import List from '../List/List';
@@ -15,9 +13,7 @@ import styles from './Profiles.module.css';
 
 const Profiles = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [profileToRenameKey, setProfileToRenameKey] = useState();
   const { items: profiles, save, deleteItems: deleteProfile } = useObjectStore('profiles');
-  const formRef = useRef();
 
   const confirmAndDelete = (key) => {
     // eslint-disable-next-line no-alert
@@ -35,38 +31,6 @@ const Profiles = () => {
     }
   };
 
-  const renameProfile = async (event) => {
-    event.preventDefault();
-
-    const { name } = event.target;
-
-    const profile = profiles.find((profile) => profile.key === profileToRenameKey);
-
-    await save({ key: profile.key, value: { ...profile.value, name: name.value } });
-
-    setProfileToRenameKey(undefined);
-  };
-
-  const showRenameInput = useCallback(
-    (key) => {
-      setProfileToRenameKey(key);
-      setTimeout(() => {
-        const formElement = formRef.current.base;
-        const nameInputElement = formElement.elements.name;
-
-        // focus the name input
-        nameInputElement.focus();
-      }, 10);
-    },
-    [formRef]
-  );
-
-  const triggerRename = useCallback(() => {
-    const formElement = formRef.current.base;
-
-    formElement.submit();
-  }, [formRef]);
-
   return (
     <>
       <PageTitle>Profiles</PageTitle>
@@ -76,44 +40,19 @@ const Profiles = () => {
           data on a single device.
         </p>
         {Boolean(profiles) && (
-          <Form name="renameProfile" onSubmit={renameProfile} ref={formRef}>
-            <List>
-              {profiles.map(({ key, value: { name, isSelected } }) => (
-                <ListItem key={key} className={styles.profileItem}>
-                  {profileToRenameKey === key ? (
-                    <Input
-                      name="name"
-                      required="required"
-                      showRequired={false}
-                      value={name}
-                      hideLabel
-                    >
-                      Name
-                    </Input>
-                  ) : (
-                    <span class={styles.name}>{name}</span>
-                  )}
-                  <Button
-                    onClick={() => {
-                      if (profileToRenameKey !== key) {
-                        showRenameInput(key);
-                      } else {
-                        triggerRename();
-                      }
-                    }}
-                  >
-                    {profileToRenameKey !== key ? 'Rename' : 'Save'}
-                  </Button>
-                  <Button disabled={isSelected} onClick={() => selectProfile(key)}>
-                    Select
-                  </Button>
-                  <Button disabled={isSelected} ghost onClick={() => confirmAndDelete(key)}>
-                    X
-                  </Button>
-                </ListItem>
-              ))}
-            </List>
-          </Form>
+          <List>
+            {profiles.map(({ key, value: { name, isSelected } }) => (
+              <ListItem key={key} className={styles.profileItem}>
+                <span class={styles.name}>{name}</span>
+                <Button disabled={isSelected} onClick={() => selectProfile(key)}>
+                  Select
+                </Button>
+                <Button disabled={isSelected} ghost onClick={() => confirmAndDelete(key)}>
+                  X
+                </Button>
+              </ListItem>
+            ))}
+          </List>
         )}
         <Button emphasized onClick={() => setIsModalOpen(true)}>
           Add +
