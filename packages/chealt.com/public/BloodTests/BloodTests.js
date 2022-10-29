58813d380e84c9894b718b49e95585cbc12e5b30
Attila Bartha <atikenny@gmail.com>
2022-10-29 19:43:50 +0200

    allow saving blood type

--- packages/chealt.com/public/BloodTests/BloodTests.js
+++ packages/chealt.com/public/BloodTests/BloodTests.js
@@ -1,5 +1,55 @@
-import PageTitle from '../PageTitle/PageTitle';
+import { useContext } from 'preact/hooks';
 
-const BloodTests = () => <PageTitle>Blood Tests</PageTitle>;
+import { AppState } from '../App/state';
+import Button from '../Form/Button';
+import Form from '../Form/Form';
+import Option from '../Form/Option';
+import Select from '../Form/Select';
+import { useObjectStore } from '../IndexedDB/hooks';
+import PageTitle from '../PageTitle/PageTitle';
+import { findDetails } from './utils';
+
+const bloodTypes = ['A', 'AB', 'B', 'O'];
+
+const BloodTests = () => {
+  const {
+    profiles: { selectedProfileId }
+  } = useContext(AppState);
+  const { save, items } = useObjectStore('blood');
+  const profileBloodDetails = findDetails(items, selectedProfileId.value);
+
+  const saveBloodType = async (event) => {
+    event.preventDefault();
+
+    const {
+      bloodType: { value: bloodType }
+    } = event.target;
+    const key = profileBloodDetails.key || crypto.randomUUID();
+
+    await save({ key, value: { bloodType, profileId: selectedProfileId.value } });
+  };
+
+  return (
+    <>
+      <PageTitle>Blood Tests</PageTitle>
+      <Form name="bloodType" onSubmit={saveBloodType}>
+        <Select name="bloodType" label="Blood type">
+          {bloodTypes.map((type) => (
+            <Option
+              key={type}
+              value={type}
+              selected={profileBloodDetails.value?.bloodType === type}
+            >
+              {type}
+            </Option>
+          ))}
+        </Select>
+        <Button type="submit" emphasized>
+          Save
+        </Button>
+      </Form>
+    </>
+  );
+};
 
 export default BloodTests;
