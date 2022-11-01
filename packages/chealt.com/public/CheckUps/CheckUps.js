150e10d2f4cc720d9fc415f9bad02529f9dac5bf
Attila Bartha <atikenny@gmail.com>
2022-10-31 22:29:43 -0400

    add check up list styles

--- packages/chealt.com/public/CheckUps/CheckUps.js
+++ packages/chealt.com/public/CheckUps/CheckUps.js
@@ -1,6 +1,9 @@
+import { useLocation } from 'preact-iso';
 import { useContext } from 'preact/hooks';
 
 import { AppState } from '../App/state';
+import Button from '../Form/Button';
+import Launch from '../Icons/Launch';
 import { useObjectStore } from '../IndexedDB/hooks';
 import List from '../List/List';
 import ListItem from '../List/ListItem';
@@ -8,10 +11,13 @@ import PageTitle from '../PageTitle/PageTitle';
 import BloodType from './BloodType';
 import { byProfileId, withCheckUpTag } from './utils';
 
+import styles from './CheckUps.module.css';
+
 const CheckUps = () => {
   const {
     profiles: { selectedProfileId }
   } = useContext(AppState);
+  const { route } = useLocation();
   const { items: documents } = useObjectStore('documents');
   const checkUps = documents?.filter(byProfileId(selectedProfileId.value))?.filter(withCheckUpTag);
 
@@ -25,7 +31,17 @@ const CheckUps = () => {
         <section>
           <List isSimple={false}>
             {checkUps.map((checkUp) => (
-              <ListItem key={checkUp.key}>{checkUp.value.name}</ListItem>
+              <ListItem key={checkUp.key} className={styles.checkUp}>
+                <div class={styles.name}>{checkUp.value.name}</div>
+                <Button
+                  ghost
+                  onClick={() => {
+                    route(`/documents/view/${btoa(checkUp.key)}`);
+                  }}
+                >
+                  <Launch />
+                </Button>
+              </ListItem>
             ))}
           </List>
         </section>
