8824d56b5101690745c956221bc8025b700a7e93
Attila Bartha <atikenny@gmail.com>
2023-04-21 14:57:17 +0200

    move nav to side panel

--- packages/chealt.com/public/Header/Header.js
+++ packages/chealt.com/public/Header/Header.js
@@ -1,25 +1,67 @@
+import classnames from 'classnames';
+import { useRef } from 'preact/hooks';
 import { useTranslation } from 'preact-i18next';
 
+import Button from '../Form/Button';
+import Menu from '../Icons/Menu';
 import ProfilePictureMenu from '../ProfilePicture/ProfilePictureMenu';
 
 import styles from './Header.module.css';
 
 const Header = () => {
   const { t } = useTranslation();
+  const dialogRef = useRef();
+  const navItems = [
+    { href: '/', label: t('pages.home.nav') },
+    { href: '/documents', label: t('pages.documents.nav') },
+    { href: '/share', label: t('pages.share.nav') },
+    { href: '/personal-details', label: t('pages.personalDetails.nav') },
+    { href: '/check-ups', label: t('pages.checkUps.nav') },
+    { href: '/vaccinations', label: t('pages.vaccinations.nav') },
+    { href: '/profiles', label: t('pages.profiles.nav') }
+  ];
+  const toggleMenu = () => {
+    const dialog = dialogRef.current;
+
+    if (dialog.open) {
+      dialog.close();
+    } else {
+      dialog.show();
+    }
+  };
+  const closeMenu = () => {
+    dialogRef.current.close();
+  };
 
   return (
-    <header class={styles.header}>
-      <nav>
-        <a href="/">{t('pages.home.nav')}</a>
-        <a href="/documents">{t('pages.documents.nav')}</a>
-        <a href="/share">{t('pages.share.nav')}</a>
-        <a href="/personal-details">{t('pages.personalDetails.nav')}</a>
-        <a href="/check-ups">{t('pages.checkUps.nav')}</a>
-        <a href="/vaccinations">{t('pages.vaccinations.nav')}</a>
-        <a href="/profiles">{t('pages.profiles.nav')}</a>
-      </nav>
-      <ProfilePictureMenu />
-    </header>
+    <>
+      <header class={styles.header}>
+        <Button ghost hideLabel label={t('common.menu')} onClick={toggleMenu}>
+          <Menu />
+        </Button>
+        <div class={styles.profileContainer}>
+          <ProfilePictureMenu />
+        </div>
+      </header>
+      <dialog
+        id="navigationMenu"
+        ref={dialogRef}
+        class={classnames({
+          [styles.menu]: true
+        })}
+      >
+        <nav class={styles.nav}>
+          <Button ghost className={styles.closeMenuContainer} onClick={closeMenu}>
+            <Menu />
+          </Button>
+          {navItems.map(({ href, label }) => (
+            <a key={href} href={href} onClick={closeMenu}>
+              {label}
+            </a>
+          ))}
+        </nav>
+      </dialog>
+    </>
   );
 };
 
--- packages/chealt.com/public/Icons/Menu.js
+++ packages/chealt.com/public/Icons/Menu.js
@@ -1,5 +1,5 @@
 const Menu = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
     <path d="M4 6h24v2H4zM4 24h24v2H4zM4 12h24v2H4zM4 18h24v2H4z" />
     <path data-name="&lt;Transparent Rectangle&gt;" style="fill:none" d="M0 0h32v32H0z" />
   </svg>
