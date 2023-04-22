55d77c2ce6467de5b91b113af26f37a6c0759152
Attila Bartha <atikenny@gmail.com>
2023-04-22 19:19:00 +0200

    set url for main branch UI tests (#188)

--- packages/chealt.com/config.js
+++ packages/chealt.com/config.js
@@ -1,5 +1,9 @@
-const url = process.env.CI
-  ? `https://chealt-git-${process.env.GITHUB_HEAD_REF}-chealt.vercel.app`
-  : 'http://localhost:8080';
+const isLocal = !process.env.CI;
+const vercelUrl =
+  process.env.GITHUB_HEAD_REF === 'main'
+    ? 'https://chealt.com'
+    : `https://chealt-git-${process.env.GITHUB_HEAD_REF}-chealt.vercel.app`;
+
+const url = isLocal ? 'http://localhost:8080' : vercelUrl;
 
 export { url };
