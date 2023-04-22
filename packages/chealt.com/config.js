d1ab18079ecc7635261296d2ee24b3bba4793649
Attila Bartha <atikenny@gmail.com>
2023-04-22 19:37:39 +0200

    on the main branch GITHUB_HEAD_REF is missing

--- packages/chealt.com/config.js
+++ packages/chealt.com/config.js
@@ -1,8 +1,7 @@
 const isLocal = !process.env.CI;
-const vercelUrl =
-  process.env.GITHUB_HEAD_REF === 'main'
-    ? 'https://chealt.com'
-    : `https://chealt-git-${process.env.GITHUB_HEAD_REF}-chealt.vercel.app`;
+const vercelUrl = !process.env.GITHUB_HEAD_REF
+  ? 'https://chealt.com'
+  : `https://chealt-git-${process.env.GITHUB_HEAD_REF}-chealt.vercel.app`;
 
 const url = isLocal ? 'http://localhost:8080' : vercelUrl;
 
