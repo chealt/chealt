8a47f5167afdf293b2fc0bbbde0a7c97fb99e2d7
Attila Bartha <atikenny@gmail.com>
2024-01-15 15:30:21 +0100

    remove issue creation

--- packages/chealt.com/public/Home/Home.js
+++ packages/chealt.com/public/Home/Home.js
@@ -1,6 +1,5 @@
-import { Trans, useTranslation } from 'preact-i18next';
+import { useTranslation } from 'preact-i18next';
 
-import Link from '../Link/Link';
 import PageTitle from '../PageTitle/PageTitle';
 
 const Home = () => {
@@ -13,28 +12,6 @@ const Home = () => {
       <p>{t('pages.home.mainDescription')}</p>
       <h2>{t('pages.home.privacyTitle')}</h2>
       <p>{t('pages.home.privacyDescription')}</p>
-      <h2>{t('pages.home.improvementsTitle')}</h2>
-      <p>
-        <Trans i18nKey="pages.home.improvementsDescription">
-          If you have an idea how to make this app even more useful, let us know
-          <Link
-            target="_blank"
-            href="https://github.com/chealt/chealt/issues/new?assignees=atikenny&labels=component:chealt.com&template=feature_request.md&title="
-            rel="noreferrer"
-          >
-            here
-          </Link>
-          by creating a
-          <Link
-            target="_blank"
-            href="https://docs.github.com/en/issues/tracking-your-work-with-issues/about-issues"
-            rel="noreferrer"
-          >
-            GitHub issue
-          </Link>
-          .
-        </Trans>
-      </p>
     </>
   );
 };
