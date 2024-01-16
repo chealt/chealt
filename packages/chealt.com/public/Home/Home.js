10921ace6eeb89f79f50250e0b864708524425a9
Attila Bartha <atikenny@gmail.com>
2024-01-16 16:34:55 +0100

    add Documents to home feed

--- packages/chealt.com/public/Home/Home.js
+++ packages/chealt.com/public/Home/Home.js
@@ -29,6 +29,13 @@ const Home = () => {
             {t('common.add')}
           </Button>
         </Tile>
+        <Tile>
+          <TileTitle capitalize>{t('pages.documents.title')}</TileTitle>
+          <p>{t('pages.home.documents.description')}</p>
+          <Button isLink href={'/documents'}>
+            {t('common.add')}
+          </Button>
+        </Tile>
       </TileList>
       <h2>{t('pages.home.privacyTitle')}</h2>
       <p>{t('pages.home.privacyDescription')}</p>
