41e1be47d27cb99b49e1327329df705b8adf9eae
Attila Bartha <atikenny@gmail.com>
2024-06-11 15:10:45 +0200

    move public and src

--- packages/chealt.com/src/index.js
+++ packages/chealt.com/src/index.js
@@ -1,13 +1,13 @@
 import 'preact/debug';
 import { hydrate, prerender as ssr } from 'preact-iso';
-import swURL from 'sw:./sw.js'; // eslint-disable-line import/no-unresolved
+// import swURL from 'sw:./sw.js'; // eslint-disable-line import/no-unresolved
 
 import App from './App/App';
 
 // SSR
-if (typeof navigator !== 'undefined') {
-  navigator.serviceWorker.register(swURL);
-}
+// if (typeof navigator !== 'undefined') {
+  // navigator.serviceWorker.register(swURL);
+// }
 
 hydrate(<App />);
 
