b8f234f598071c9b8beb22320b81f739701d39ad
Attila Bartha <atikenny@gmail.com>
2024-06-21 16:32:09 +0200

    add VitePWA basic config

--- packages/chealt.com/vite.config.js
+++ packages/chealt.com/vite.config.js
@@ -1,8 +1,24 @@
 import preact from '@preact/preset-vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    // eslint-disable-next-line new-cap
+    VitePWA({
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Chealt',
+        short_name: 'Chealt', // eslint-disable-line camelcase
+        background_color: 'rgb(26 26 26)', // eslint-disable-line camelcase
+        theme_color: 'rgb(236 179 66)' // eslint-disable-line camelcase
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   esbuild: {
     loader: 'jsx'
   },
